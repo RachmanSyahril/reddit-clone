5b3021776006b3d42ddf4ee5365021a8afeb0b6e
Syahril Rachman <rachman.syahril@gmail.com>
2023-02-27 16:16:35 +0700

    added oauth buttons

---
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -0,0 +1,20 @@
+import { Button, Flex, Image } from '@chakra-ui/react';
+import React from 'react';
+
+const OAuthButtons: React.FC = () => {
+  return (
+    <Flex direction="column" width="100%" mb={4}>
+      <Button variant="oauth" mb={2}>
+        <Image
+          src="/images/googlelogo.png"
+          alt="google logo"
+          height="20px"
+        ></Image>
+        &nbsp; Continue with Google
+      </Button>
+      <Button variant="oauth">Some Other Providers</Button>
+    </Flex>
+  );
+};
+
+export default OAuthButtons;

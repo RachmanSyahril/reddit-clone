c4e72c5d9fd6820c5894a20a8597c09039336b91
Syahril Rachman <rachman.syahril@gmail.com>
2023-03-10 18:11:07 +0700

    NewPostForm 1

---
+++ src/components/Posts/NewPostForm.tsx
@@ -0,0 +1,38 @@
+import { Flex, Icon } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import { BsLink45Deg, BsMic } from 'react-icons/bs';
+import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
+import TabItem from './TabItem';
+
+type NewPostFormProps = {};
+
+const formTabs = [
+  { title: 'Post', icon: IoDocumentText },
+  { title: 'Images & Video', icon: IoImageOutline },
+  { title: 'Link', icon: BsLink45Deg },
+  { title: 'Talk', icon: BsMic },
+];
+
+export type TabItemType = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm: React.FC<NewPostFormProps> = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
+  return (
+    <Flex direction="column" bg="white" borderRadius={4} mt={2}>
+      <Flex width="100%">
+        {formTabs.map((item) => (
+          <TabItem
+            item={item}
+            key={item.title}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
+        ))}
+      </Flex>
+    </Flex>
+  );
+};
+export default NewPostForm;

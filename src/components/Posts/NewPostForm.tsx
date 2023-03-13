99a90e00891e93387e0423fdb09d4a68a1fc85e9
Syahril Rachman <rachman.syahril@gmail.com>
2023-03-13 10:59:39 +0700

    added posts functionality

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,24 @@
-import { Flex, Icon } from '@chakra-ui/react';
+import { Post } from '@/src/atoms/postsAtom';
+import { firestore, storage } from '@/src/firebase/clientApp';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Flex,
+  Icon,
+  Text,
+} from '@chakra-ui/react';
+import { User } from 'firebase/auth';
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from 'firebase/firestore';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { BsLink45Deg, BsMic } from 'react-icons/bs';
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5';
@@ -6,7 +26,9 @@ import ImageUpload from './PostsForm/ImageUpload';
 import TextInputs from './PostsForm/TextInputs';
 import TabItem from './TabItem';
 
-type NewPostFormProps = {};
+type NewPostFormProps = {
+  user: User;
+};
 
 const formTabs = [
   { title: 'Post', icon: IoDocumentText },
@@ -20,16 +42,58 @@ export type TabItemType = {
   icon: typeof Icon.arguments;
 };
 
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+  const router = useRouter();
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
-  const [textInput, setTextInput] = useState({
+  const [textInputs, setTextInputs] = useState({
     title: '',
     body: '',
   });
   const [selectedFile, setSelectedFile] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const handleCreatePost = async () => {};
+  const handleCreatePost = async () => {
+    const { communityId } = router.query;
+
+    // create new post object => type post
+    const newPost: Post = {
+      communityId: communityId as string,
+      creatorId: user.uid,
+      creatorDisplayName: user.email!.split('@')[0],
+      title: textInputs.title,
+      body: textInputs.body,
+      numberOfComments: 0,
+      voteStatus: 0,
+      createdAt: serverTimestamp() as Timestamp,
+    };
+
+    setLoading(true);
+    try {
+      // store the post in db
+      const postDocRef = await addDoc(collection(firestore, 'posts'), newPost);
+
+      // check for selectedFile
+      if (selectedFile) {
+        // store it in the storage => getDownloadURL (return imageURL)
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectedFile, 'data_url');
+        const downloadURL = await getDownloadURL(imageRef);
+
+        // update post doc by adding imageURL
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+      }
+    } catch (error: any) {
+      console.error('handleCreatePost', error.message);
+      setError(true);
+    }
+    setLoading(false);
+
+    // redirect user back to the communityPage using the router
+    router.back();
+  };
 
   const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
     const reader = new FileReader();
@@ -52,7 +116,7 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
       target: { name, value },
     } = event;
 
-    setTextInput((prev) => ({
+    setTextInputs((prev) => ({
       ...prev,
       [name]: value,
     }));
@@ -73,7 +137,7 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
       <Flex p={4}>
         {selectedTab === 'Post' && (
           <TextInputs
-            textInputs={textInput}
+            textInputs={textInputs}
             handleCreatePost={handleCreatePost}
             onChange={onTextChange}
             loading={loading}
@@ -88,6 +152,12 @@ const NewPostForm: React.FC<NewPostFormProps> = () => {
           />
         )}
       </Flex>
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          <Text mr={2}>Error creating post</Text>
+        </Alert>
+      )}
     </Flex>
   );
 };
